80c6745187fa9ce42864b586301503e24649d963
sandip vaghasiya <hi.shah31@gmail.com>
2019-12-11 17:07:39 +0100

    inital commit

---
+++ src/test/java/com/cloud/config/SpringBootMicroserviceCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cloud.config;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMicroserviceCloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
